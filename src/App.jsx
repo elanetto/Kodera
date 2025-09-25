8922b0d45619e72698ab5bada869fb95809c087e
elanetto <101054274+elanetto@users.noreply.github.com>
2025-09-25 21:06:00 +0200

    Lagt til karusell komponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import "./index.css";
 import SortLogo from "./assets/branding/kodera-logo-01.svg";
 import Offers from "./components/offers";
+import { CarouselComponent } from "./Components/Carousel";
+
+import carouselImage1 from "./assets/carousel/main/Karusellbilde1.jpg";
+import carouselImage2 from "./assets/carousel/main/Karusellbilde2.jpg";
+import carouselImage3 from "./assets/carousel/main/Karusellbilde3.jpg";
+
+import portfolioImage1 from "./assets/carousel/portfolio/anettetherese.jpg"
+import portfolioImage2 from "./assets/carousel/portfolio/elanettoNO-anette.jpg"
+import portfolioImage3 from "./assets/carousel/portfolio/holidaze-anette.jpg"
 
 function App() {
+  const carouselImages = [
+  { src: carouselImage1, alt: "First slide", caption: "Vi designer nettsider for din bedrift" },
+  { src: carouselImage2, alt: "Second slide", caption: "Programmerer unike nettsider for ditt behov" },
+  { src: carouselImage3, alt: "Third slide", caption: "Velkommen til Kodera: Kvinner i tech", Link: "/omOss" }
+];
+
+
+
+  const portfolioImages = [
+  { src: portfolioImage1, alt: "Anette Therese Portfolio", caption: "Portfolioside: Anette Therese, FrontEnd", link: "https://anette-portfolio.onrender.com/" },
+  { src: portfolioImage2, alt: "elanetto Stickers", caption: "elanetto Design - en liten klistremerkebedrift", link: "https://www.elanetto.no/" },
+  { src: portfolioImage3, alt: "Holidaze Booking: Exam Project", caption: "Holidaze - En booking nettside", link: "https://dev-holiday.onrender.com/" }
+];
+
+
   return (
     <div className="flex items-center flex-col">
-      <div className="max-w-5xl w-full flex justify-center flex-col items-center gap-5 h-105">
+      <div className="w-full">
+        <CarouselComponent images={carouselImages} title="Kodera main carousel" />
+      </div>
+      <div className="max-w-5xl w-full flex justify-center flex-col items-center gap-5 h-full">
         <h1 className="text-xl text-coal">Hei hei! Her kommer...</h1>
 
         <img
@@ -19,6 +46,9 @@ function App() {
         Dette kan vi tilby
       </h2>
       <Offers />
+      <div className="w-full pt-10">
+        <CarouselComponent images={portfolioImages} title="Portfolio carousel" />
+      </div>
     </div>
   );
 }
