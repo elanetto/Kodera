3554349d900d26643621f6827c0e992156f48d82
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2025-09-27 15:01:08 +0200

    la til en </div> som manglet. fjernet duplicate <offers />

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,58 @@ import carouselImage1 from "./assets/carousel/main/Karusellbilde1.jpg";
 import carouselImage2 from "./assets/carousel/main/Karusellbilde2.jpg";
 import carouselImage3 from "./assets/carousel/main/Karusellbilde3.jpg";
 
-import portfolioImage1 from "./assets/carousel/portfolio/anettetherese.jpg"
-import portfolioImage2 from "./assets/carousel/portfolio/elanettoNO-anette.jpg"
-import portfolioImage3 from "./assets/carousel/portfolio/holidaze-anette.jpg"
+import portfolioImage1 from "./assets/carousel/portfolio/anettetherese.jpg";
+import portfolioImage2 from "./assets/carousel/portfolio/elanettoNO-anette.jpg";
+import portfolioImage3 from "./assets/carousel/portfolio/holidaze-anette.jpg";
 
 function App() {
   const carouselImages = [
-  { src: carouselImage1, alt: "First slide", caption: "Vi designer nettsider for din bedrift" },
-  { src: carouselImage2, alt: "Second slide", caption: "Programmerer unike nettsider for ditt behov" },
-  { src: carouselImage3, alt: "Third slide", caption: "Velkommen til Kodera: Kvinner i tech", Link: "/omOss" }
-];
+    {
+      src: carouselImage1,
+      alt: "First slide",
+      caption: "Vi designer nettsider for din bedrift",
+    },
+    {
+      src: carouselImage2,
+      alt: "Second slide",
+      caption: "Programmerer unike nettsider for ditt behov",
+    },
+    {
+      src: carouselImage3,
+      alt: "Third slide",
+      caption: "Velkommen til Kodera: Kvinner i tech",
+      Link: "/omOss",
+    },
+  ];
 
   const portfolioImages = [
-  { src: portfolioImage1, alt: "Anette Therese Portfolio", caption: "Portfolioside: Anette Therese, FrontEnd", link: "https://anette-portfolio.onrender.com/" },
-  { src: portfolioImage2, alt: "elanetto Stickers", caption: "elanetto Design - en liten klistremerkebedrift", link: "https://www.elanetto.no/" },
-  { src: portfolioImage3, alt: "Holidaze Booking: Exam Project", caption: "Holidaze - En booking nettside", link: "https://dev-holiday.onrender.com/" }
-];
-
+    {
+      src: portfolioImage1,
+      alt: "Anette Therese Portfolio",
+      caption: "Portfolioside: Anette Therese, FrontEnd",
+      link: "https://anette-portfolio.onrender.com/",
+    },
+    {
+      src: portfolioImage2,
+      alt: "elanetto Stickers",
+      caption: "elanetto Design - en liten klistremerkebedrift",
+      link: "https://www.elanetto.no/",
+    },
+    {
+      src: portfolioImage3,
+      alt: "Holidaze Booking: Exam Project",
+      caption: "Holidaze - En booking nettside",
+      link: "https://dev-holiday.onrender.com/",
+    },
+  ];
 
   return (
     <div className="flex items-center flex-col">
       <div className="w-full">
-        <CarouselComponent images={carouselImages} title="Kodera main carousel" />
+        <CarouselComponent
+          images={carouselImages}
+          title="Kodera main carousel"
+        />
       </div>
       <div className="max-w-5xl w-full flex justify-center flex-col items-center gap-5 h-full">
         <h1 className="text-xl text-coal">Hei hei! Her kommer...</h1>
@@ -50,9 +80,13 @@ function App() {
       </h2>
       <div className="w-full">
         <ContactForm />
-        <Offers />
-      <div className="w-full pt-10">
-        <CarouselComponent images={portfolioImages} title="Portfolio carousel" />
+
+        <div className="w-full pt-10">
+          <CarouselComponent
+            images={portfolioImages}
+            title="Portfolio carousel"
+          />
+        </div>
       </div>
     </div>
   );
