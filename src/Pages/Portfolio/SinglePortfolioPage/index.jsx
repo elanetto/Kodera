7fcd775de54758944dacf9775ee22eabaeba4502
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-02-28 00:26:12 +0100

    fjernet scroll på høyre kort portfolio enn så lenge

--- src/Pages/Portfolio/SinglePortfolioPage/index.jsx
+++ src/Pages/Portfolio/SinglePortfolioPage/index.jsx
@@ -55,12 +55,13 @@ export default function Project() {
       <div className="max-w-7xl mx-auto  px-4 md:px-8  lg:px-20 xl:px-4 pt-8 lg:pt-12">
         <div className="grid gap-8 xl:grid-cols-[minmax(0,2fr)_minmax(0,1.3fr)]  xl:h-[420px] 2xl:h-[580px]">
           {/* LEFT: Carousel */}
-          <div className="h-full min-h-0">
+          <div className="h-full ">
             <CarouselComponent images={carouselImages} title={project.title} />
           </div>
 
           {/* RIGHT: Info panel */}
-          <div className="h-full min-h-0  ">
+          <div className="h-full  ">
+            {/* min-h-0  hvis man ønsker scroll*/}
             <div className="bg-coal text-paperwhite border border-white rounded-2xl p-6 md:p-7 shadow-xl h-full flex flex-col items-center sm:items-start text-center sm:text-left min-h-0">
               <h2 className="text-2xl  lg:text-3xl font-medium   font-oswald">
                 {project.title}
