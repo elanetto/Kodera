1acd40c840d9c323c200864bd179f6718566f48f
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-05-08 21:13:19 +0200

    added fancy loader. in layout.

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from "react";
+import { Outlet, useNavigation, useLocation } from "react-router-dom";
+import Lottie from "lottie-react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import { Outlet } from "react-router-dom";
 import AnalyticsHelper from "../Components/AnalyticsHelper";
 import ScrollToTop from "../Components/ScrollToTop";
+import koderaLoader from "./../assets/Loader/kodera-loader.json";
 
 export default function Layout() {
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => setLoading(false), 800);
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
   return (
     <div className="flex flex-col min-h-screen">
       <ScrollToTop />
       <Header />
       <AnalyticsHelper />
 
-      {/* 👇 push all page content down so it doesn't hide behind fixed header */}
+      {loading && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
+          <Lottie
+            animationData={koderaLoader}
+            loop={true}
+            className="w-60 h-60"
+          />
+        </div>
+      )}
+
+      {/*  pushing all  content down so it doesn't hide behind fixed header */}
       <main className="flex-1 pt-20 sm:pt-24">
         <Outlet />
       </main>
